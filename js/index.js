498bc2e71f3de5a17e9e7818285dc8030c2ee6cb
Soojin Choi <51164164+suzyn@users.noreply.github.com>
2026-09-01 20:22:05 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,33 @@
 
+// $(document).ready(function() {
+//     $('.publication-mousecell').mouseover(function() {
+//         $(this).find('video').css('display', 'inline-block');
+//         $(this).find('img').css('display', 'none');
+//     });
+//     $('.publication-mousecell').mouseout(function() {
+//         $(this).find('video').css('display', 'none');
+//         $(this).find('img').css('display', 'inline-block');
+//     });
+// })
+
 $(document).ready(function() {
     $('.publication-mousecell').mouseover(function() {
         $(this).find('video').css('display', 'inline-block');
         $(this).find('img').css('display', 'none');
     });
+
     $('.publication-mousecell').mouseout(function() {
         $(this).find('video').css('display', 'none');
         $(this).find('img').css('display', 'inline-block');
     });
-})
+
+    $('#news-toggle').click(function() {
+        $('.news-hidden').toggle();
+
+        if ($(this).text().trim() === 'Show earlier updates') {
+            $(this).text('Hide earlier updates');
+        } else {
+            $(this).text('Show earlier updates');
+        }
+    });
+});
